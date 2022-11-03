c71694419adfceed7acd9cef461562c7ab496ac8
Alberto Ola <alberto.ola.dev@gmail.com>
2022-11-03 01:07:37 -0600

    chore: background and colores for dark mode and light mode. Delegate TextField Methods included

--- Weather/Controller/WeatherViewController.swift
+++ Weather/Controller/WeatherViewController.swift
@@ -8,17 +8,42 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        let place = searchTextField.text!
+        print(place)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        let place = searchTextField.text!
+        print(place)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        }
+        
+        textField.placeholder = "Type something"
+        return false
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
 }
 
